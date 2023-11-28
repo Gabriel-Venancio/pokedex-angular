8df09c92b65a7df93218c76d63a35499bce4d5cb
Gabriel Venancio <Gabriel@DESKTOP-QQFFJ0U>
2023-11-28 15:24:44 -0300

    feat: pokedex com angular

---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Pokemon } from 'src/_model/Pokemon';
+import { Type } from 'src/_model/Type';
+import { PokemonService } from 'src/_service/pokemon.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.sass']
+})
+export class PokemonListComponent {
+  constructor(
+    public pokemonService: PokemonService,
+  ) {
+    
+  }
+}
